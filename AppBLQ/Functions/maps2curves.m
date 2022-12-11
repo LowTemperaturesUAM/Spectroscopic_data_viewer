67cb18f01bac9a027a698186fadc5f6a0f7762ed
Miguel Agueda <miguelagve2@gmail.com>
2022-12-11 23:36:16 +0100

    Initial commit for treating curve data as maps. Added functions to convert between both formats.

---
+++ AppBLQ/Functions/maps2curves.m
@@ -0,0 +1,10 @@
+function [Matriz] = maps2curves(Cell,Info)
+    pts = numel(Info.Voltaje);
+    Row = numel(Info.DistanciaFilas);
+    Col = numel(Info.DistanciaColumnas);
+    MatrizPrevia = cell2mat(Cell);
+    MatrizPrevia = pagetranspose(MatrizPrevia);
+    MatrizPrevia = permute(MatrizPrevia,[3,1,2]);
+    Matriz = reshape(MatrizPrevia,[pts,Col*Row]);
+    clear MatrizPrevia pts Row Col
+end
