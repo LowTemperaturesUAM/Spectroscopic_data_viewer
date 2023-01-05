39cc9a060d5712f0dcc751905a7c5597e2b46223
Miguel Agueda <miguelagve2@gmail.com>
2023-01-05 19:30:57 +0100

    Real space dimensions are now calculated correctly when cutting an InfoStruct A warning is displayed when trying to cut the maps with uneven dimensions. Significant time reduction when cutting an InfoStruct. Improvements to FFT tab in blqApp -Reordering of buttons for better keyboard navigation -Addition of discrete buttons for 90 degree rotations in both directions Improvements to analyze app -Reordering of buttons in analyze app for improved keyboard navigation

--- AppBLQ/Functions/RecorteQPI.m
+++ AppBLQ/Functions/RecorteQPI.m
@@ -9,12 +9,13 @@ InfoStruct1.MapasConductancia = Mapas1;
 InfoStruct1.Transformadas = Transformadas1;
 
 InfoStruct1.TamanhoRealColumnas = Info.TamanhoRealColumnas*...
-    length(InfoStruct1.MapasConductancia{1})/length(Info.MapasConductancia{1});
+    size(InfoStruct1.MapasConductancia{1},1)/size(Info.MapasConductancia{1},1);
 InfoStruct1.TamanhoRealFilas = Info.TamanhoRealFilas*...
-    length(InfoStruct1.MapasConductancia{1})/length(Info.MapasConductancia{1});
+    size(InfoStruct1.MapasConductancia{1},2)/size(Info.MapasConductancia{1},2);
 
-Filas = length(Info.DistanciaFilas);
-Columnas = length(Info.DistanciaColumnas);
+%Obtain the size from the cut down maps
+Filas = size(InfoStruct1.MapasConductancia{1},1);
+Columnas = size(InfoStruct1.MapasConductancia{1},2);
 
 TamanhoPixelFilas = InfoStruct1.TamanhoRealFilas/Filas;
 TamanhoPixelColumnas = InfoStruct1.TamanhoRealColumnas/Columnas;
@@ -34,25 +35,44 @@ InfoStruct1.DistanciaFourierFilas = (1/InfoStruct1.TamanhoRealFilas)*(1:1:Filas)
     InfoStruct1.DistanciaFourierFilas = InfoStruct1.DistanciaFourierFilas - InfoStruct1.DistanciaFourierFilas(floor(Filas/2)+1);
 %-------------------------------------------------------------------------
 
-Filas = length(Info.DistanciaFilas);
-Columnas = length(Info.DistanciaColumnas);
+OldFilas = length(Info.DistanciaFilas);
+OldColumnas = length(Info.DistanciaColumnas);
 PuntosIV = length(Info.Voltaje);
-
-for k=1:PuntosIV
-    Matriz3D(:,:,k) = reshape(Info.MatrizNormalizada(k,:),Filas,Columnas);
-%     Matriz3D(:,:,k) = Matriz3D(:,:,k)';
-    Matriz3DRecortada(:,:,k) = Matriz3D(y1:y2,x1:x2,k);
-    MatrizNormalizadaRecortada (k,:) = reshape(Matriz3DRecortada(:,:,k),1,(x2-x1+1)*(y2-y1+1));
-end
-
-for k=1:PuntosIV
-    Matriz3D(:,:,k) = reshape(Info.MatrizCorriente(k,:),Filas,Columnas);
-%     Matriz3D(:,:,k) = Matriz3D(:,:,k)';
-    Matriz3DRecortada(:,:,k) = Matriz3D(y1:y2,x1:x2,k);
-    MatrizCorrienteRecortada (k,:) = reshape(Matriz3DRecortada(:,:,k),1,(x2-x1+1)*(y2-y1+1));
-end
-
-% length(MatrizNormalizadaRecortada)
+% tic
+% for k=1:PuntosIV %very slow
+%     Matriz3D(:,:,k) = reshape(Info.MatrizNormalizada(k,:),OldFilas,OldColumnas);
+% %     Matriz3D(:,:,k) = Matriz3D(:,:,k)';
+%     Matriz3DRecortada(:,:,k) = Matriz3D(y1:y2,x1:x2,k);
+%     MatrizNormalizadaRecortada(k,:) = reshape(Matriz3DRecortada(:,:,k),1,(x2-x1+1)*(y2-y1+1));
+% end
+% toc
+tic
+% pts = numel(Info.Voltaje);
+Matriz3D = reshape(Info.MatrizNormalizada,[PuntosIV,OldColumnas,OldFilas]);
+Matriz3D = permute(Matriz3D,[2,3,1]);
+Matriz3D = pagetranspose(Matriz3D);
+Matriz3DRecortada = Matriz3D(y1:y2,x1:x2,:);
+Matriz3DRecortada = pagetranspose(Matriz3DRecortada);
+Matriz3DRecortada = permute(Matriz3DRecortada,[3,1,2]);
+MatrizNormalizadaRecortada = reshape(Matriz3DRecortada,[PuntosIV,Filas*Columnas] );
+toc
+% tic
+% for k=1:PuntosIV
+%     Matriz3D(:,:,k) = reshape(Info.MatrizCorriente(k,:),OldFilas,OldColumnas);
+% %     Matriz3D(:,:,k) = Matriz3D(:,:,k)';
+%     Matriz3DRecortada(:,:,k) = Matriz3D(y1:y2,x1:x2,k);
+%     MatrizCorrienteRecortada (k,:) = reshape(Matriz3DRecortada(:,:,k),1,(x2-x1+1)*(y2-y1+1));
+% end
+% toc
+tic
+Matriz3D = reshape(Info.MatrizCorriente,[PuntosIV,OldColumnas,OldFilas]);
+Matriz3D = permute(Matriz3D,[2,3,1]);
+Matriz3D = pagetranspose(Matriz3D);
+Matriz3DRecortada = Matriz3D(y1:y2,x1:x2,:);
+Matriz3DRecortada = pagetranspose(Matriz3DRecortada);
+Matriz3DRecortada = permute(Matriz3DRecortada,[3,1,2]);
+MatrizCorrienteRecortada = reshape(Matriz3DRecortada,[PuntosIV,Filas*Columnas] );
+toc
 InfoStruct1.MatrizNormalizada = MatrizNormalizadaRecortada;
 InfoStruct1.MatrizCorriente = MatrizCorrienteRecortada;
 InfoStruct = InfoStruct1;
