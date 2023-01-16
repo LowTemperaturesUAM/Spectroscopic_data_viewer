d73c442785aaab082747655b0a069c6c3a93c336
Miguel Agueda <miguelagve2@gmail.com>
2023-01-16 21:11:05 +0100

    Fixed an incorrect index order in RecorteQPI introduced with the previous changes. Refactoring of the function

--- AppBLQ/Functions/RecorteQPI.m
+++ AppBLQ/Functions/RecorteQPI.m
@@ -1,50 +1,34 @@
-function [InfoStruct] = RecorteQPI(y1,y2,x1,x2,Info)
-for k=1:length(Info.Energia)
-    Mapas1{k} = Info.MapasConductancia{k}(x1:x2,y1:y2);
-    Transformadas1{k} = fft2d(Mapas1{k});
-end
-
-InfoStruct1 = Info;
-InfoStruct1.MapasConductancia = Mapas1;
-InfoStruct1.Transformadas = Transformadas1;
-
-InfoStruct1.TamanhoRealColumnas = Info.TamanhoRealColumnas*...
-    size(InfoStruct1.MapasConductancia{1},1)/size(Info.MapasConductancia{1},1);
-InfoStruct1.TamanhoRealFilas = Info.TamanhoRealFilas*...
-    size(InfoStruct1.MapasConductancia{1},2)/size(Info.MapasConductancia{1},2);
-
+function [InfoStruct] = RecorteQPI(x1,x2,y1,y2,Info)
+%Get the previous dimensions of the data
+[OldFilas,OldColumnas] = size(Info.MapasConductancia{1});
+PuntosIV = length(Info.Voltaje);
+%Cut the maps and obtain the new fft maps
+Info.MapasConductancia = cellfun(@(x) x(y1:y2,x1:x2),...
+    Info.MapasConductancia,UniformOutput=false);
+Info.Transformadas = cellfun(@(x) fft2d(x),...
+    Info.MapasConductancia,UniformOutput=false);
 %Obtain the size from the cut down maps
-Filas = size(InfoStruct1.MapasConductancia{1},1);
-Columnas = size(InfoStruct1.MapasConductancia{1},2);
+[Filas,Columnas] = size(Info.MapasConductancia{1});
 
-TamanhoPixelFilas = InfoStruct1.TamanhoRealFilas/Filas;
-TamanhoPixelColumnas = InfoStruct1.TamanhoRealColumnas/Columnas;
+Info.TamanhoRealColumnas = Info.TamanhoRealColumnas*Columnas/OldColumnas;
+Info.TamanhoRealFilas = Info.TamanhoRealFilas*Filas/OldFilas;
 
-Filas = length(Mapas1{1});
-Columnas = Filas;
 
-InfoStruct1.DistanciaColumnas = TamanhoPixelColumnas*(1:1:Columnas);
-InfoStruct1.DistanciaFilas = TamanhoPixelFilas*(1:1:Filas);
+TamanhoPixelFilas = Info.TamanhoRealFilas/Filas;
+TamanhoPixelColumnas = Info.TamanhoRealColumnas/Columnas;
+
+Info.DistanciaColumnas = TamanhoPixelColumnas*(1:1:Columnas);
+Info.DistanciaFilas = TamanhoPixelFilas*(1:1:Filas);
     
-InfoStruct1.DistanciaFourierColumnas = (1/InfoStruct1.TamanhoRealColumnas)*(1:1:Columnas);
-InfoStruct1.DistanciaFourierFilas = (1/InfoStruct1.TamanhoRealFilas)*(1:1:Filas);
+Info.DistanciaFourierColumnas = (1/Info.TamanhoRealColumnas)*(1:1:Columnas);
+Info.DistanciaFourierFilas = (1/Info.TamanhoRealFilas)*(1:1:Filas);
 
 % Centering reciprocal space units
 % ------------------------------------------------------------------------
-	InfoStruct1.DistanciaFourierColumnas = InfoStruct1.DistanciaFourierColumnas - InfoStruct1.DistanciaFourierColumnas(floor(Columnas/2)+1); 
-    InfoStruct1.DistanciaFourierFilas = InfoStruct1.DistanciaFourierFilas - InfoStruct1.DistanciaFourierFilas(floor(Filas/2)+1);
+Info.DistanciaFourierColumnas = Info.DistanciaFourierColumnas - Info.DistanciaFourierColumnas(floor(Columnas/2)+1);
+Info.DistanciaFourierFilas = Info.DistanciaFourierFilas - Info.DistanciaFourierFilas(floor(Filas/2)+1);
 %-------------------------------------------------------------------------
 
-OldFilas = length(Info.DistanciaFilas);
-OldColumnas = length(Info.DistanciaColumnas);
-PuntosIV = length(Info.Voltaje);
-
-% for k=1:PuntosIV %very slow
-%     Matriz3D(:,:,k) = reshape(Info.MatrizNormalizada(k,:),OldFilas,OldColumnas);
-% %     Matriz3D(:,:,k) = Matriz3D(:,:,k)';
-%     Matriz3DRecortada(:,:,k) = Matriz3D(y1:y2,x1:x2,k);
-%     MatrizNormalizadaRecortada(k,:) = reshape(Matriz3DRecortada(:,:,k),1,(x2-x1+1)*(y2-y1+1));
-% end
 
 % should use a function for the conversion instead
 Matriz3D = reshape(Info.MatrizNormalizada,[PuntosIV,OldColumnas,OldFilas]);
@@ -56,14 +40,6 @@ Matriz3DRecortada = permute(Matriz3DRecortada,[3,1,2]);
 MatrizNormalizadaRecortada = reshape(Matriz3DRecortada,[PuntosIV,Filas*Columnas] );
 
 
-% for k=1:PuntosIV
-%     Matriz3D(:,:,k) = reshape(Info.MatrizCorriente(k,:),OldFilas,OldColumnas);
-% %     Matriz3D(:,:,k) = Matriz3D(:,:,k)';
-%     Matriz3DRecortada(:,:,k) = Matriz3D(y1:y2,x1:x2,k);
-%     MatrizCorrienteRecortada (k,:) = reshape(Matriz3DRecortada(:,:,k),1,(x2-x1+1)*(y2-y1+1));
-% end
-
-
 Matriz3D = reshape(Info.MatrizCorriente,[PuntosIV,OldColumnas,OldFilas]);
 Matriz3D = permute(Matriz3D,[2,3,1]);
 Matriz3D = pagetranspose(Matriz3D);
@@ -72,7 +48,7 @@ Matriz3DRecortada = pagetranspose(Matriz3DRecortada);
 Matriz3DRecortada = permute(Matriz3DRecortada,[3,1,2]);
 MatrizCorrienteRecortada = reshape(Matriz3DRecortada,[PuntosIV,Filas*Columnas] );
 
-InfoStruct1.MatrizNormalizada = MatrizNormalizadaRecortada;
-InfoStruct1.MatrizCorriente = MatrizCorrienteRecortada;
-InfoStruct = InfoStruct1;
+Info.MatrizNormalizada = MatrizNormalizadaRecortada;
+Info.MatrizCorriente = MatrizCorrienteRecortada;
+InfoStruct = Info;
 end
