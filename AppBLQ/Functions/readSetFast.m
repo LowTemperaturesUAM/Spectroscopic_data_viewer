1f8989dfeae574f8be5bb96905247ea55a003875
Miguel Agueda <miguelagve2@gmail.com>
2023-01-17 15:52:16 +0100

    Fixed issue with blqReaderV16 ignoring the curve scale factor

--- AppBLQ/Functions/readSetFast.m
+++ AppBLQ/Functions/readSetFast.m
@@ -1,15 +1,16 @@
 %Esto telee columnas del BLQ, ya sean voltaje o corriente, o cualquier otra
 %cosa. Vale para BLQs de cualquier electronica. 
 
-function [Data, readFlag] = readSetFast(FileID, NumeroFilas,DataFormat)%,HeaderSizeDataFormat)
+function [Data, readFlag] = readSetFast(FileID, NumeroFilas,DataFormat,Factor)
         %En una espectro, toda esta informacion deberia ser la misma en
         %cada curva, y estamos gastando el 70% del tiempo de ejecucion de
         %esta funcion en ello
         fseek(FileID,128,'cof'); %skip everything 
-        Factor = 1;
-%         fseek(FileID,20,'cof');
-%         Factor = fread(FileID,1,'float64')
-%         fseek(FileID,100,'cof');
+        % If we want to get the prefactor for this particular curve, we do this
+        % instead of skipping everything
+        % fseek(FileID,20,'cof');
+        % Factor = fread(FileID,1,'float64')
+        % fseek(FileID,100,'cof');
         switch DataFormat % Abrimos la IV propiamente dicha y extraemos sus datos en la matriz 'Data'
         	case 0
             	Data = Factor*(Offset + Start + (Size/NumeroFilas)*(0:(NumeroFilas-1)));   
