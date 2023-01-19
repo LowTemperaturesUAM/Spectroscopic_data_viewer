ed3d597f28651b9bcb044f2e492b8550ed981561
Miguel Agueda <miguelagve2@gmail.com>
2023-01-19 17:46:25 +0100

    Initial work for allowing odd number of points in FFT images

---
+++ AppBLQ/Functions/RotateMatrixFFT.m
@@ -0,0 +1,11 @@
+function [CellRotated] = RotateMatrixFFT(Cell, Angle)
+
+%if the matrix has even number of elements, we add an extra element at the
+%end to keep the position of the center unchanged during the rotation
+if mod(Filas,2)==0 && mod(Columnas,2) == 0
+    CellRotated = cellfun(@(x) imrotate(x([1:end 1],[1:end 1]),Angle,'crop')...
+        ,Cell,UniformOutput=false);
+    CellRotated = cellfun(@(x) x(1:end-1,1:end-1),CellRotated,UniformOutput=false);
+else
+    CellRotated = cellfun(@(x) imrotate(x,Angle,'crop'),Cell,UniformOutput=false);
+end
