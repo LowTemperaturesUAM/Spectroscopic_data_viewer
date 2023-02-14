8533501e615d35bd671de23d6219f877cfacc65b
Pablo García <110162220+pablogarciat@users.noreply.github.com>
2023-02-14 18:41:33 +0100

    Fixed issue when defining Contrast Limits
    Contrast slider limits prealocated to [0,1] so that they can be easily displaced.
    initCell now defines new Limits as the widest interval.

--- AppBLQ/Functions/initCell.m
+++ AppBLQ/Functions/initCell.m
@@ -10,7 +10,9 @@ else %FFT
     minValue = 0; %always fix the lower limit to zero for FFT
 end
     maxValue = max(maxVector);
-    %Compare this to the current slider limits and take the wider
-    values = chooseContrast([minValue maxValue],app.MinSlider.Limits(1),app.MinSlider.Limits(2));
+    %Compare this to the current slider limits and take the widest
+    %values = chooseContrast([minValue maxValue],app.MinSlider.Limits(1),app.MinSlider.Limits(2));
+    values = sort([minValue maxValue app.MinSlider.Limits],"ascend");
+    values = values([1,end]);
     [app.MinSlider.Limits,app.MaxSlider.Limits] = deal(values);
 end
