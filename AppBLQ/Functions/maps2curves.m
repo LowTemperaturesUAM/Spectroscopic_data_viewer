081975c8a53e75fabd11efaaa40211742872a2a0
Miguel Agueda <miguelagve2@gmail.com>
2023-03-20 22:55:36 +0100

    Small improvements to map-curve conversion. The format for the cell array now matches other struct fields.

--- AppBLQ/Functions/maps2curves.m
+++ AppBLQ/Functions/maps2curves.m
@@ -2,6 +2,7 @@ function [Matriz] = maps2curves(Cell,Info)
     pts = numel(Info.Voltaje);
     Row = numel(Info.DistanciaFilas);
     Col = numel(Info.DistanciaColumnas);
+    Cell = reshape(Cell,[1 1 numel(Cell)]);
     MatrizPrevia = cell2mat(Cell);
     MatrizPrevia = pagetranspose(MatrizPrevia);
     MatrizPrevia = permute(MatrizPrevia,[3,1,2]);
