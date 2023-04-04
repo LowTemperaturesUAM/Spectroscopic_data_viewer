a90389355d03b7f8243fa67a257eb8a38e7d31c9
Miguel Agueda <miguelagve2@gmail.com>
2023-04-04 02:18:30 +0200

    Added the option to read topographic data from IMG files -Fixed some issues with the scale when reading old STP filed exported from viewIMG

--- AppBLQ/Functions/ReadTopoV2.m
+++ AppBLQ/Functions/ReadTopoV2.m
@@ -25,16 +25,52 @@ if isequal(Name,0)
     TopoLineas=128; %Valor de puntos por defecto en este caso
     Matrix = [];
 else
+
     Ext = Name(end-2:end); %extension del archivo de imagen
     if strcmp(Ext,'img') %Solo sacamos el numero de puntos
         disp('Img file loaded')
         Struct.FileNameTopo = Name;
         Struct.FilePathTopo = Path;
-        TopoProperties      = dir([Path Name]);
-        TopoLineas          = sqrt((TopoProperties.bytes - 1032)/4);
-        Matrix = [];
+%         TopoProperties      = dir([Path Name]);
+%         TopoLineas          = sqrt((TopoProperties.bytes - 1032)/4);
+        FileIMG=fopen([Path Name],'r');
+        % identifier and version
+        fread(FileIMG,2,'int32');
+        % day, month and year
+        fread(FileIMG,3,'uint16');
+        % moment and time (seconds of day and final-start)
+        fread(FileIMG,2,'double');
+        % Xstart, Xend in meters
+        RealXLim = fread(FileIMG,2,'double');
+        % Ystart, Yend in meters
+        RealYLim = fread(FileIMG,2,'double');
+        Struct.TamanhoRealColumnas = diff(RealXLim)*1e9;
+        Struct.TamanhoRealFilas = diff(RealYLim)*1e9;
+        Msize = fread(FileIMG,2,'int32=>double');
+        if Msize(1)==Msize(2)
+            TopoLineas=Msize(1);
+        end
+        % empty space
+        fread(FileIMG,450,'char*1');
+        % Comment
+        fread(FileIMG,512,'char*1');
+
+        disp(readall)
+        if readall
+            Matrix = fread(FileIMG,Msize.','single')*1e9;
+            Matrix = rot90(Matrix,-1);
+            %Shift to positive values
+            Matrix = Matrix - min(Matrix,[],"all");
+        else
+            %If we don't want the whole image, or we don't now the proper
+            %format, we return an empty matrix
+            Matrix = [];
+        end
+
     elseif strcmp(Ext,'stp') %Sacamos numero de puntos y tamaño
         disp('STP file loaded')
+        Struct.FileNameTopo = Name;
+        Struct.FilePathTopo = Path;
         FileSTP=fopen([Path Name],'r');
         %No podemos usar el tamaño del header porque es incorrecto en
         %algunas versiones de MyScanner
@@ -106,8 +142,7 @@ else
                             %break
                         case 'Image Data Type'
                             %Las imagenes antiguas vienen etiquetadas como
-                            %single, y contienen la informacion directa del
-                            % DAC Z como int16
+                            %single, pero son int16
                             switch C{2}{:}
                                 case 'single' %Imagenes antiguas
                                     type = 'int16';
@@ -135,7 +170,7 @@ else
                                     fprintf('This unit cannot be read!!')
                                     Factor = 1;
                             end
-                            Zscale = Number * Factor;
+                            Zscale = Number * Factor
                     end
 
                     tline=fgetl(FileSTP);
@@ -150,15 +185,16 @@ else
                 % For new images from MyScanner, the format is indeed double, as
                 % the header says. We just need to offset the lowest point
                 Matrix = fread(FileSTP,[TopoLineas, TopoLineas],type);
-                Matrix = Matrix - min(Matrix,[],"all");
             else
-                % For old images exported in STP from viewIMG, the format is the
-                % raw 16bit DAC information, despite the header saying is single
-                Matrix = fread(FileSTP,[TopoLineas, TopoLineas],type);
-                Matrix = Matrix - min(Matrix,[],"all");
-                Matrix = Matrix / max(Matrix,[],"all");
+                % For old images exported in STP from viewIMG, the format 
+                % is int16, despite the header saying is single
+                % an additional 1e-3 factor has to be used, for some reason
+                % but it doesn't seem to get it right with all images
+                Matrix = fread(FileSTP,[TopoLineas, TopoLineas],type)*1e-3;
             end
             %This part is the same for both cases
+            %Shift to positive values
+            Matrix = Matrix - min(Matrix,[],"all");
             Matrix = Matrix * Zscale;
             Matrix = rot90(Matrix,-1);
         else
@@ -171,6 +207,8 @@ else
         TopoLineas=128;
         Matrix = [];
     end
+
+
 end
 
 end
