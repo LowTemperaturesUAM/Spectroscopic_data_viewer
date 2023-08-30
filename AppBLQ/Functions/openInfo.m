1485d01f3a7641067271c1ac945002a1184b80f6
Miguel Agueda <miguelagve2@gmail.com>
2023-08-30 20:25:33 +0200

    Reworked data loading process. The process should now be more robust and will give more helpful information when an error is raised.

--- AppBLQ/Functions/openInfo.m
+++ AppBLQ/Functions/openInfo.m
@@ -1,21 +1,13 @@
-%Abre un archivo InfoStruct y lo almacena en una variable interna de la
-%aplicación [Info] y en el workspace general [InfoStructOriginal]
-function [Info,Settings] = openInfo()
-[File,Path] = uigetfile('*.mat');
-% disp(File)
-if ~isequal(File,0)
-    File = load([Path,File]);
+function [Info,Settings] = openInfo(File,Path)
+File = load([Path,File]);
+if isfield(File,'InfoStruct') && isstruct(File.InfoStruct)
     Info = File.InfoStruct;
-    if isfield(File,'Struct')%exist('File.Struct','var')
-        Settings = File.Struct;
-        %disp('Loaded analysis info')
-    else
-        Settings = struct();
-        %disp('No file information available')
-    end
 else
-    Settings = struct();
-    Info= struct();
-    % assignin('base','InfoStructOriginal',Info);
+    Info = struct([]);
+end
+if isfield(File,'Struct') && isstruct(File.Struct)
+    Settings = File.Struct;
+else
+    Settings = struct([]);
 end
 end
--- AppBLQ/Functions/readInfo.m
+++ AppBLQ/Functions/readInfo.m
@@ -1,3 +1,22 @@
-function [Info] = readInfo()
-Info = evalin('base', 'InfoStruct');
+function [Info,Settings] = readInfo()
+try
+    Var1 = evalin('base', 'InfoStruct');
+    if isstruct(Var1)
+        Info = Var1;
+    else
+        Info = struct([]);
+    end
+catch
+    Info = struct([]);
+end
+try
+    Var2 = evalin('base', 'Struct');
+    if isstruct(Var2)
+        Settings = Var2;
+    else
+        Settings = struct([]);
+    end
+catch
+    Settings = struct([]);
+end
 end
