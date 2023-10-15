08595f0c3b2a9a29368a589f7c5d6913423cda02
Miguel Agueda <miguelagve2@gmail.com>
2023-10-15 18:58:32 +0200

    Small fix to popcorn function to avoid NaN values Improvments to ifft2d function. It now allows any kind of image as an input, with the option to enforce real output as an explicit flag

--- AppBLQ/Functions/ifft2d.m
+++ AppBLQ/Functions/ifft2d.m
@@ -1,8 +1,23 @@
-function [RSI] = ifft2d(MODULE,PHASE)
-% Returns the inverse FFT of an image, given its module and phase information
-% This function assumes the real space data is not complex, and enforces
-% the symmetry when doing the calculation
-   FullFFT = MODULE.*exp(1j*PHASE);
-   %reverse the applied shift and calculate the ifft
-   RSI = ifft2(ifftshift(FullFFT),"symmetric");
+function [RSI] = ifft2d(MODULE,PHASE,opt)
+arguments
+    MODULE (:,:) double {mustBeReal,mustBeNonnegative,mustBeFinite}
+    PHASE (:,:) double {mustBeReal,mustBeFinite} = 0
+    opt.Real logical = false;
+end
+% Calculates the Inverse Fourier Transform on a given 2D map
+% The function assumes that the data is increasingly ordered in momentum
+% with the zero frecuency component located in the
+% [floor(row)/2+1,floor(column)/2+1] point
+% It takes the module and phase components as separate arguments
+% If no phase is provided, it is assumed to be zero for all points
+% Optionally, the argument Real = true can be passed if the real space data
+% is not complex so that the symmetry is enforced on the calculation
+
+FullFFT = MODULE.*exp(1j*PHASE);
+%reverse the applied shift and calculate the ifft
+if opt.Real %set the flag to true if the real space data is real
+    RSI = ifft2(ifftshift(FullFFT),"symmetric");
+else
+    RSI = ifft2(ifftshift(FullFFT));
+end
 end
--- AppBLQ/Functions/popcorn.m
+++ AppBLQ/Functions/popcorn.m
@@ -1,13 +1,13 @@
-function CellPopcorn = popcorn(Cell,sigma,Info)
+function CellPopcorn = popcorn(Cell,sigma,~)
 CellPopcorn = Cell;
 
 for k=1:length(Cell)
     MatrizTopo = Cell{k};
-    L=length(MatrizTopo);
-    M=mean(mean(MatrizTopo));
+    L=size(MatrizTopo,1);
+    M=mean(MatrizTopo,"all");
     S=std(MatrizTopo,1,'all');
     MatrizTopoCorr=MatrizTopo;
-    BadPointsMatrix=ones(L);
+    BadPointsMatrix=ones(L,'logical');
     for i=2:L-1
         for j=2:L-1
             if MatrizTopo(i,j) > M+sigma*S || MatrizTopo(i,j) < M-sigma*S
@@ -15,16 +15,25 @@ for k=1:length(Cell)
             end
         end
     end
+
     for i=2:L-1
         for j=2:L-1
             if BadPointsMatrix(i,j)==0
                 neigh=MatrizTopo(i-1:i+1,j-1:j+1).*BadPointsMatrix(i-1:i+1,j-1:j+1);
-                MatrizTopoCorr(i,j)=sum(sum(neigh))/(sum(sum(BadPointsMatrix(i-1:i+1,j-1:j+1))));
+                window = sum(BadPointsMatrix(i-1:i+1,j-1:j+1),"all");
+                if window ==0
+                    MatrizTopoCorr(i,j) = 0;
+                    MatrizTopoCorr(i,j) = sum(MatrizTopoCorr(i-1:i,j-1:j),"all")/3;
+                else
+                MatrizTopoCorr(i,j)=sum(neigh,"all")/(sum(BadPointsMatrix(i-1:i+1,j-1:j+1),"all"));
+                end
             else
                 MatrizTopoCorr(i,j)=MatrizTopo(i,j);
             end
         end
     end
+%     assert(all(~(MatrizTopoCorr > M+sigma*S | MatrizTopoCorr < M-sigma*S),'all'),'Values are outside the specified limits')
+    assert(any(isnan(MatrizTopoCorr),'all'),'The matrix includes some NaN values')
     CellPopcorn{k}=MatrizTopoCorr;
 end
 
