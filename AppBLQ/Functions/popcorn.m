a03463abc38f492bf52c7d7c50ac57420c18a520
Miguel Agueda <miguelagve2@gmail.com>
2023-10-22 22:22:03 +0200

    Fix issue with latest update to popcorn filter

--- AppBLQ/Functions/popcorn.m
+++ AppBLQ/Functions/popcorn.m
@@ -18,14 +18,14 @@ for k=1:length(Cell)
 
     for i=2:L-1
         for j=2:L-1
-            if BadPointsMatrix(i,j)==0
+            if BadPointsMatrix(i,j)
                 neigh=MatrizTopo(i-1:i+1,j-1:j+1).*BadPointsMatrix(i-1:i+1,j-1:j+1);
                 window = sum(BadPointsMatrix(i-1:i+1,j-1:j+1),"all");
-                if window ==0
+                if window == 0
                     MatrizTopoCorr(i,j) = 0;
                     MatrizTopoCorr(i,j) = sum(MatrizTopoCorr(i-1:i,j-1:j),"all")/3;
                 else
-                MatrizTopoCorr(i,j)=sum(neigh,"all")/(sum(BadPointsMatrix(i-1:i+1,j-1:j+1),"all"));
+                    MatrizTopoCorr(i,j)=sum(neigh,"all")/(sum(BadPointsMatrix(i-1:i+1,j-1:j+1),"all"));
                 end
             else
                 MatrizTopoCorr(i,j)=MatrizTopo(i,j);
@@ -33,7 +33,7 @@ for k=1:length(Cell)
         end
     end
 %     assert(all(~(MatrizTopoCorr > M+sigma*S | MatrizTopoCorr < M-sigma*S),'all'),'Values are outside the specified limits')
-    assert(any(isnan(MatrizTopoCorr),'all'),'The matrix includes some NaN values')
+    assert(~any(isnan(MatrizTopoCorr),'all'),'The matrix includes some NaN values')
     CellPopcorn{k}=MatrizTopoCorr;
 end
 
