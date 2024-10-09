2c010b1be9cff727705dc87579b6d594add40562
Miguel <miguelagve2@gmail.com>
2024-10-09 10:41:37 +0200

    Implemented smoothing functions in real units instead of pixels for consistency

---
+++ AppBLQ/Functions/GaussSmoothV2.m
@@ -0,0 +1,53 @@
+function FilterMaps = GaussSmoothV2(Maps,row,col,sigma,opts)
+arguments
+    Maps
+    row double {mustBeVector}
+    col double {mustBeVector}
+    sigma double {mustBePositive,mustBeFinite}
+    opts.Padding string {mustBeMember(opts.Padding,{'circular','replicate','symmetric','value','average'})} = 'replicate'
+    opts.PaddingValue (1,1) double {mustBeFinite} = 0;
+end
+%convert the width of the distribution to pixel units
+if numel(sigma)>2
+    error('The standard deviation of the filter must be a scalar or a 2 element vector')
+end
+drow = abs(row(2)-row(1));
+dcol = abs(col(2)-col(1));
+srow = sigma(1)/drow;
+scol = sigma(end)/dcol;
+
+%Check if the standard deviation is square or not
+if abs((srow-scol)/scol)<1e-3
+    sigmapix = srow;
+else
+    sigmapix = [srow,scol];
+end
+meanFlag = false;
+switch opts.Padding
+    case {'circular','replicate','symmetric'}
+        Method = opts.Padding;
+    case 'average'
+        meanFlag = true;
+    otherwise
+        Method = opts.PaddingValue;
+end
+if iscell(Maps)
+    %assume all maps in the cell have equal size
+    Size = floor(size(Maps{1})/2)+1; %Half the image size
+    if meanFlag
+        FilterMaps = cellfun(@(M) imgaussfilt(M,sigmapix,Padding=mean(M,"all"),...
+            FilterSize=Size),Maps,UniformOutput=false);
+    else
+        FilterMaps = cellfun(@(M) imgaussfilt(M,sigmapix,Padding=Method,FilterSize=Size),...
+            Maps,UniformOutput=false);
+    end
+elseif ismatrix
+    Size = floor(size(Maps)/2)+1;
+    if meanFlag 
+        Method = mean(M,"all");
+    end
+    FilterMaps = imgaussfilt(Maps,sigmapix,Padding=Method,FilterSize=Size);
+else
+    error('The provided input is not an array or a cell array')
+end
+end
---
+++ AppBLQ/Functions/gaussCoreSubstractionV2.m
@@ -0,0 +1,39 @@
+function [FilterMaps] = gaussCoreSubstractionV2(Maps,row,col,sigma)
+arguments
+    Maps
+    row double {mustBeVector}
+    col double {mustBeVector}
+    sigma double {mustBePositive,mustBeFinite}
+end
+% Applies a filter to remove a gaussian core from an FFT map or a cell array of
+% FFT maps. The maps are expected to be fftshifted beforehand
+
+%Convert Sigma to pixels 
+
+drow = abs(row(2)-row(1));
+dcol = abs(col(2)-col(1));
+srow = sigma(1)/drow;
+scol = sigma(end)/dcol;
+%Check if the standard deviation is square or not
+if abs((srow-scol)/scol)<1e-3
+    sigmapix = srow;
+else
+    sigmapix = [srow,scol];
+end
+
+
+x0 = find(row == 0);
+y0 = find(col == 0);
+x = (1:1:numel(row));
+y = (1:1:numel(col))';
+MatrizFiltroFFT = -expm1(-((x-x0)/(sqrt(2)*sigmapix)).^2-((y-y0)/(sqrt(2)*sigmapix)).^2);
+
+if iscell(Maps)
+    FilterMaps = cellfun(@(x) x.*MatrizFiltroFFT,Maps,UniformOutput=false);
+elseif ismatrix(Maps)
+    FilterMaps = Maps.*MatrizFiltroFFT;
+else
+    error('The provided input is not an array or a cell array')
+end
+
+end
