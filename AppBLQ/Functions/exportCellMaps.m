f0ceaf857e113bd6841bad0878b62d287b614674
Miguel <miguelagve2@gmail.com>
2024-11-13 15:03:39 +0100

    Added option to export images as a video

--- AppBLQ/Functions/exportCellMaps.m
+++ AppBLQ/Functions/exportCellMaps.m
@@ -38,23 +38,13 @@ elseif Format == 4 %Make Video with Maps as frames. Default 7 FPS
     mapVideo(Cell,Contrast,Energy,Colormap, ...
         'Filename',[Path,filesep,options.Prefix,'MapVideo','.','avi'], ...
         'Framerate',7,'ColorbarVisible',false,'getFrame','figure');
+    return
 end
 
 NCell = length(Info.Energia);
 SaveFigure = figure;
 SaveFigure.Visible = 'off';
 colormap(Info.Colormap);
-% if isReal %Real
-%     imagesc(Info.DistanciaColumnas, Info.DistanciaFilas, Cell{1});
-%     SaveFigure.Children.CLim = Info.ContrastReal(:,1);
-%     SaveFigure.Children.XLim =  Info.XLimReal;
-%     SaveFigure.Children.YLim =  Info.YLimReal;
-% else %FFT
-%     imagesc(Info.DistanciaFourierColumnas, Info.DistanciaFourierFilas, Cell{1});
-%     SaveFigure.Children.CLim = Info.ContrastFFT(:,1);
-%     SaveFigure.Children.XLim =  Info.XLimFFT;
-%     SaveFigure.Children.YLim =  Info.YLimFFT;
-% end
 imagesc(X,Y,Cell{1})
 SaveFigure.Children.CLim = Contrast(:,1);
 SaveFigure.Children.XLim =  XLim;
@@ -64,29 +54,9 @@ SaveFigure.Children.FontSize = 20;
 SaveFigure.Children.DataAspectRatio = [1,1,1];
 SaveFigure.Children.TickDir = 'out';
 SaveFigure.Children.XTickLabel = SaveFigure.Children.YTickLabel;
-% switch Format
-%     case 0 % NO TITLE NO AXES NO BORDERS
-% %         SaveFigure.Children.Position =  SaveFigure.Children.OuterPosition;
-%         SaveFigure.Children.XTick = [];
-%         SaveFigure.Children.YTick = [];
-%         SaveFigure.Children.XTickLabel = [];
-%         SaveFigure.Children.YTickLabel = [];
-
-%     case 1 % ONLY TITLE
-%         title([num2str(Info.Energia(1)), ' mV']);
-%         SaveFigure.Children.XTick = [];
-%         SaveFigure.Children.YTick = [];
-%         SaveFigure.Children.XTickLabel = [];
-%         SaveFigure.Children.YTickLabel = [];
-
-%     case 2 % WITH TITLE AND AXES
-%         title([num2str(Info.Energia(1)), ' mV']);
-
-    %Add anothe option for addin just a scalebar?
-% end
-
 title([num2str(Info.Energia(1)), ' mV']);
-if Format == 1
+
+if Format == 1 %Disable axis if specified
     SaveFigure.Children.XTick = [];
     SaveFigure.Children.YTick = [];
     SaveFigure.Children.XTickLabel = [];
@@ -96,17 +66,9 @@ end
 for i = 1:NCell
     SaveFigure.Children.Children.CData = Cell{i};
     SaveFigure.Children.CLim = Contrast(:,i);
-%     if isReal %Real
-%         SaveFigure.Children.CLim = Info.ContrastReal(:,i);
-%     else %FFT
-%         SaveFigure.Children.CLim = Info.ContrastFFT(:,i);
-%     end
     name = num2str(Info.Energia(i));
     name = strrep(name,'.',',');
     title([name,' mV'])
-%     if Format == 1 || Format ==2
-%         title([name,' mV'])
-%     end
     exportgraphics(SaveFigure.Children,[Path,filesep,options.Prefix,...
         name,'.',options.FileType],'Resolution',250)
 end
