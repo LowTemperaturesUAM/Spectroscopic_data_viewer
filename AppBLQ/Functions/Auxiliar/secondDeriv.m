728e191aa111f7b7256def4da9481e56264ce0aa
Pablo García <110162220+pablogarciat@users.noreply.github.com>
2025-01-21 13:47:56 +0100

    2nd derivative maps now show at current Energy
    - Comments added

--- AppBLQ/Functions/Auxiliar/secondDeriv.m
+++ AppBLQ/Functions/Auxiliar/secondDeriv.m
@@ -4,10 +4,20 @@ arguments
     Info struct
     opts.Voltage double = 0
 end
+% Obtain Rows and Columns from a Map matrix
 [Row,Col] = size(Info.MapasConductancia{1});
-dMap = singleDerivMap(Info.Voltaje,Info.MatrizNormalizada,...
-    derivPts,opts.Voltage,Row,Col);
 
+% In case there is an open map, use current energy for new Map
+if ~isempty(findall(0,'Name','Analyze Real Maps'))
+    f = findall(0,'Name','Analyze Real Maps');
+    opts.Voltage = findobj(f(1),'Type','uiSpinner','BackgroundColor','k').Value;
+end
+
+% Calculate map of 2nd derivative
+dMap = singleDerivMap(Info.Voltaje,Info.MatrizNormalizada,...
+    derivPts,opts.Voltage(1),Row,Col);
+
+% Create figure with map and histogram
 f = figure(Name='Second derivative');
 f.Position(3:4)=[1000,435];
 tiledlayout(1,2,TileSpacing="compact",Padding="compact")
@@ -17,6 +27,7 @@ set(gca,'YDir','normal','DataAspectRatio',[1 1 1])
 crameri('vik','pivot',0)
 colorbar
 axis off
+title(opts.Voltage(1)+" meV") % Indicate map energy
 
 nexttile
 histogram(dMap)
--- AppBLQ/Functions/Auxiliar/singleDerivMap.m
+++ AppBLQ/Functions/Auxiliar/singleDerivMap.m
@@ -1,6 +1,13 @@
 function SlopeMap = singleDerivMap(V,Matrix,derivPts,Vmap,Row,Col)
+% SlopeMap = singleDerivMap(V,Matrix,derivPts,Vmap,Row,Col) calculates the
+% map matrix SlopeMap(RowxCol) from the curves in Matrix with variable V.
+% Values for map are interpolateed at a value Vmap.
+
+% Calculate derivative from curves in Matrix, with variable V
     dMatrix = derivadorLeastSquaresArray(derivPts,Matrix,V);
+    % Obtain interpolated values of derivated curves at voltage Vmap
     dV = interp1(V,dMatrix,Vmap,"makima");
+
     %We convert to a map similarly to how we do in curves2maps
     SlopeMap = reshape(dV,[1,Col,Row]);
     SlopeMap = permute(SlopeMap,[2,3,1]);
