19918bc013e26ca67cc06fcbde3eb6f8846edd2b
Pablo García <110162220+pablogarciat@users.noreply.github.com>
2025-03-21 16:19:18 +0100

    Added general Brillouin Zone function
    - Added contextual Menu in Hexagonal BZ for UTe2

---
+++ AppBLQ/Functions/Auxiliar/wignerSeitz2D_V2.m
@@ -0,0 +1,41 @@
+function [P] = wignerSeitz2D_V2(Bragg)
+arguments
+    Bragg (:,2) double
+end
+% Given a set of Bragg peaks, wignerSeitz2D(Bragg) calculates the vertices of a WignerSeitz
+% (Brillouin in reciprocal space) cell around the origin, i.e. the area 
+% enclosed by these Bragg peaks whose points lie closer to the origin than
+% to any of the peaks.
+
+% INPUT
+% Bragg is a 2 column matrix with coordinates of the peaks
+
+%Example: BrillouinPeaks = wignerSeitz_V2([0 1;1 0]).
+
+Zero = mean(Bragg,1); %Center of polygon as center of mass
+Bragg = Bragg - Zero;
+
+% Sort peaks by angle
+a = atan2d(Bragg(:,2),Bragg(:,1));
+ [~,idx] = sort(a);
+ Bragg = Bragg(idx,:);
+
+
+normBragg = 0.5*vecnorm(Bragg,2,2).^2;
+% Number of peaks
+numPeaks = numel(normBragg);
+
+% Solve linear system for every adjacent pair of peaks
+P = zeros(size(Bragg));
+for i = 1:numPeaks
+    j = mod(i,numPeaks)+1;
+
+    b = [normBragg(i); normBragg(j)];
+    A = [Bragg(i,1), Bragg(i,2);
+     Bragg(j,1), Bragg(j,2)];
+
+    P(i,:) = (A\b).';
+end
+% Return polygon to original position
+P = P + Zero;
+end
