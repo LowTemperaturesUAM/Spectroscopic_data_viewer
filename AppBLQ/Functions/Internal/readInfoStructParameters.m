6c28eed309e94d0448277e4aeb741eda07ee9f8c
Miguel <miguelagve2@gmail.com>
2026-05-13 18:07:15 +0200

    The information of the infostruct displayed is now more complete and easier to read

--- AppBLQ/Functions/Internal/readInfoStructParameters.m
+++ AppBLQ/Functions/Internal/readInfoStructParameters.m
@@ -11,13 +11,11 @@ if ~isempty(Struct)
     Ny = Struct.Filas;
     IV = Struct.IV;
 else
-    [Ny,Nx] = size(Info.MapasCondutancia{1});
+    [Ny,Nx] = size(Info.MapasConductancia{1});
     IV =numel(Info.Voltaje);
 end
 
 mapNr = numel(Info.MapasConductancia);
-derivPts = Info.PuntosDerivada;
-direction = Info.Direction;
 Vbias = Info.Bias;
 
 f = uifigure;
@@ -25,11 +23,11 @@ f.Name = 'Info';
 if isfield(opts,'Position')
     f.Position(1:2) = opts.Position;
 end
-f.Position(3) = 220;
-f.Position(4) = 160;
+f.Position(3) = 240;
+f.Position(4) = 190;
 topRow = f.Position(4)-30;
 hRow = 15;
-wRow = 180;
+wRow = 210;
 lPad = 20;
 uilabel(f,Position=[lPad,topRow+5, wRow, 20],...
     Text = 'Image parameters',FontWeight='bold',FontSize=15);
@@ -40,17 +38,28 @@ i = i +1;
 uilabel(f,Position=[lPad,topRow - i*hRow, wRow, 20],...
     Text = sprintf('Real size: %.2fnm x %.2fnm',Lx,Ly));
 i = i +1;
+%Get the value an change to the most fitting units
+dV = abs(diff(Info.Voltaje([1 end])))/(IV-1);
+if dV<0.1
+    CurveData = sprintf('Curve size: %d pts (%.4g \x00B5V/pt)',IV,dV*1e3);
+else
+    CurveData =sprintf('Curve size: %d pts (%.4g mV/pt)',IV,dV);
+end
 uilabel(f,Position=[lPad,topRow - i*hRow, wRow, 20],...
-    Text = sprintf('Curve size: %d pts',IV));
+    Text = CurveData);
 i = i +1;
+if isfield(Info,'Direction')
 uilabel(f,Position=[lPad,topRow - i*hRow, wRow, 20],...
-    Text = sprintf('Scan direction: %s',direction));
+    Text = sprintf('Scan direction: %s',Info.Direction));
 i = i +1;
+end
+if isfield(Info,'Type')
 uilabel(f,Position=[lPad,topRow - i*hRow, wRow, 20],...
     Text = sprintf('Map Type: %s',Info.Type));
 i = i +1;
+end
 uilabel(f,Position=[lPad,topRow - i*hRow, wRow, 20],...
-    Text = sprintf('Set Point Bias: %.2f mV',Vbias));
+    Text = sprintf('Set Point Bias: %.3g mV',Vbias));
 i = i +1;
 
 
@@ -60,6 +69,30 @@ i = i +1;
 uilabel(f,Position=[lPad,topRow - i*hRow, wRow, 20], Text = sprintf('Field: %g T',Struct.Campo));
 i = i +1;
 end
+
+if isfield(Info,'PuntosDerivada')
+    derivPts = Info.PuntosDerivada;
+    width = abs(diff(Info.Voltaje(1:2)))*derivPts;
+    if width <0.5
+        WidthData = sprintf('Energy Width: %.4g %ceV (%d pts)',width*1e3,char(0xB5),derivPts);
+    else
+        WidthData = sprintf('Energy Width: %.4g meV (%d pts)',width,derivPts);
+    end
+    uilabel(f,Position=[lPad,topRow - i*hRow, wRow, 20],Text = WidthData);
+    i = i +1;
+end
+
+dE = abs(diff(Info.Energia(1:2)));
+if dE <0.2
+    EnergyData = sprintf('Map Spacing: %.4g %ceV',dE*1e3,char(0xB5)) ;
+else
+    EnergyData = sprintf('Map Spacing: %.4g meV',dE) ;
+end
+uilabel(f,Position=[lPad,topRow - i*hRow, wRow, 20],...
+    Text = EnergyData);
+% i = i +1;
+
+
 % uiwait(f)
 
 
